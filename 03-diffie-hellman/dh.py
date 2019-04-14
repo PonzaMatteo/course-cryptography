fe09ef740cfb5335eb41dc294fe61a94d009b63f
PonzaMatteo <matteoponza@gmail.com>
2019-04-14 20:22:34 +0300

    finish diffie-hellman key exchange

--- 03-diffie-hellman/dh.py
+++ 03-diffie-hellman/dh.py
@@ -1,4 +1,5 @@
 from math import *
+from random import *
 
 def prime_numbers(low, high):
     is_prime = [True]*(high+1)
@@ -16,12 +17,12 @@ def prime_numbers(low, high):
     return primes
 
 def smallest_primitive_root(n):
-    i = 2
+    i = 1
     found = False
     spr = None
     while i < n and not found:
-        powers = {pow(i, j) % n for j in range(n-1)}
-        if len(powers) == n - 1:
+        powers = {i**j % n for j in range(n-1)}
+        if len(powers) == (n - 1):
             found = True
             spr = i
         i = i + 1
@@ -29,11 +30,36 @@ def smallest_primitive_root(n):
 
 def all_primitive_roots(n):
     spr = smallest_primitive_root(n)
-    print(spr)
     exponents = filter(lambda m: gcd(m, n - 1) == 1, range(1, n))
     aprs = [int(pow(spr, s) % n) for s in exponents]
     return aprs
         
-        
-print(smallest_primitive_root(13))
-print(all_primitive_roots(13))
+p = sample(prime_numbers(10, 100), 1)[0]
+g = sample(all_primitive_roots(p),1)[0]
+
+print("- Shared: ")
+print("p:\t",p)
+print("g:\t",g)
+
+print("- A: (send)")
+s = randint(1, p - 2)
+S = g ** s % p
+print("secret:\t", s)
+print("send:\t", S)
+
+print("- B: (send)")
+h = randint(1, p - 2)
+H = g ** h % p
+print("secret:\t", h)
+print("send:\t", H)
+
+print("- A: (receive)")
+k_a = H**s % p
+print("key:\t", k_a)
+
+print("- B: (receive)")
+k_b = S**h % p
+print("key:\t", k_b)
+
+if k_a != k_b:
+    print("Err. Something wrong?!")
